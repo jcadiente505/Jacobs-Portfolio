3de743c18f12cd8b939deeed0c48eeba7114566b
Cadman <jcadiente505@gmail.com>
2018-07-23 15:25:45 -0400

    favorite projects sidebar complete/working on the contact me section

--- assets/main.js
+++ assets/main.js
@@ -3,4 +3,14 @@ $(document).ready(function(){
       interval: 3000,
       wrap: true
     })
-  });    
+  });
+  
+  /* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
